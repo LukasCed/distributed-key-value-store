5dd23bb2d899c276960b034ba652b455c88a16d0
Lukas Cedronas <lukascedronas1@gmail.com>
2022-11-17 14:36:01 +0200

    Working tests

--- apps/kv_server/test/transaction_test.exs
+++ apps/kv_server/test/transaction_test.exs
@@ -11,44 +11,29 @@ defmodule KVStoreTest do
   setup do
     opts = [:binary, packet: :line, active: false]
     {:ok, socket} = :gen_tcp.connect('localhost', 4040, opts)
-
-    :ok = LocalCluster.start()
-    nodes = LocalCluster.start_nodes("dsn-", 3)
-    [n1, n2, n3] = nodes
-    assert Node.ping(n1) == :pong
-    assert Node.ping(n2) == :pong
-    assert Node.ping(n3) == :pong
-
-    %{socket: socket, nodes: [n1, n2, n3]}
+    %{socket: socket}
   end
 
   test "transaction commit", %{socket: socket} do
     assert String.contains?(send_and_recv(socket, "TRANSACTION\r\n"), ["OK", "started"])
-    assert String.contains?(send_and_recv(socket, "CREATE users\r\n"), ["OK", "no"])
-    assert String.contains?(send_and_recv(socket, "PUT users user1 {'name':'Ted'}\r\n"), ["OK", "no"])
-    assert String.contains?(send_and_recv(socket, "END\r\n"), ["OK", "no"])
+    assert String.contains?(send_and_recv(socket, "CREATE users\r\n"), ["OK"])
+    assert String.contains?(send_and_recv(socket, "PUT users user1 {'name':'Ted'}\r\n"), ["OK"])
+    assert String.contains?(send_and_recv(socket, "END\r\n"), ["OK"])
 
     # outside the transaction
-    assert send_and_recv(socket, "GET users user1\r\n") == "{\"name\": \"Ted\"}\r\n"
+    assert send_and_recv(socket, "GET users user1\r\n") == "OK: {'NAME':'TED'}\r\n"
   end
 
 
   test "transaction abort", %{socket: socket} do
-    assert send_and_recv(socket, "TRANSACTION\r\n") ==
-      "OK\r\n"
-    assert send_and_recv(socket, "CREATE users\r\n") ==
-          "OK\r\n"
-    assert send_and_recv(socket, "PUT users user1 {\"name\": \"Ted\"}\r\n") ==
-          "OK\r\n"
-
-    assert send_and_recv(socket, "DELETE users user2\r\n") ==
-      "OK\r\n"
-
-    assert send_and_recv(socket, "END\r\n") ==
-    "OK\r\n"
+    assert String.contains?(send_and_recv(socket, "TRANSACTION\r\n"), ["OK", "started"])
+    assert String.contains?(send_and_recv(socket, "CREATE users\r\n"), ["OK", "no"])
+    assert String.contains?(send_and_recv(socket, "PUT users user1 {'name':'Ted'}\r\n"), ["OK", "no"])
+    assert String.contains?(send_and_recv(socket, "DELETE users3 user2\r\n"), ["OK", "no"])
+    assert String.contains?(send_and_recv(socket, "END\r\n"), ["OK", "no"])
 
     # outside the transaction
-    assert send_and_recv(socket, "GET users user1\r\n") == "NOT OK\r\n"
+    assert send_and_recv(socket, "GET users user1\r\n") == "ERROR: value in table USERS with the key USER1 not found\r\n"
   end
 
   defp send_and_recv(socket, command) do
